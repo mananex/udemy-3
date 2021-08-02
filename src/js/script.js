e5c05223999a67b2005524d798ee67a15f9390bf
Harut <example@gmail.com>
2021-08-02 18:48:41 +0400

    tabs with error

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        dots: true,
+        dots: false,
         speed: 300,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron-left.png"/></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron-right.png"/></button>'
     });
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+    $('.catalog-item__link').each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('catalog-item__content').removeClass('catalog-item__content_active');
+        })
+    })
 });
